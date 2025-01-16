059f655f164d35f8fd08b958538d77768fdd2235
GuruprasadVuranduri <105349373+GuruprasadVuranduri@users.noreply.github.com>
2025-01-16 06:37:18 +0530

    changes

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Drag and Drop Grid App</h1>
+      <p>Replace this content with your components!</p>
+    </div>
+  );
+};
+
+export default App;
